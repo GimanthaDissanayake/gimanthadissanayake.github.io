9bd3f97079bf830d01da0867db8c4aaf68494e33
Gimantha Dissanayake <gimanthad@gmail.com>
2026-07-04 09:57:37 +0300

    fixed email header injection vulnerability

--- mail.php
+++ mail.php
@@ -1,12 +1,14 @@
 <?php
-$con_name = $_POST['con_name'];
-$con_email = $_POST['con_email'];
+$con_name = preg_replace('/[\r\n]/', '', $_POST['con_name']);
+$con_email = preg_replace('/[\r\n]/', '', $_POST['con_email']);
 $con_message = $_POST['con_message'];
 
+if (!filter_var($con_email, FILTER_VALIDATE_EMAIL)) {
+    echo 0;
+    exit;
+}
 
-
-
-$to = 'gimanthad@gmail.com';   
+$to = 'gimanthad@gmail.com';
 $subject = 'Web Portfolio Gimantha';
 
 $message = '<strong>Name : </strong>'.$con_name.'<br/><br/>';
@@ -18,7 +20,7 @@ $headers = 'From: '.$con_name.' '.$con_email . "\r\n" ;
 $headers .='Reply-To: '. $to . "\r\n" ;
 $headers .='X-Mailer: PHP/' . phpversion();
 $headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 
 mail($to,$subject,$message,$headers);
 echo 1;
